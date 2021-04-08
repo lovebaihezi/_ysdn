9af9e9ad227d11d4e4e76b79e3f9baac8d315be5
lovebaihezi <x18739168862@gmail.com>
2021-04-08 22:05:46 +0800

    update Ajax

--- vite-ysdn/src/component/AjaxResponse/index.tsx
+++ vite-ysdn/src/component/AjaxResponse/index.tsx
@@ -7,7 +7,12 @@ import AjaxError from '../result';
 export type AjaxProp<T> = {
     Request: useFetchProps;
     Component: Component<T>;
-    Waiting?: ({ loading }: { loading: boolean }) => JSX.Element;
+    Waiting?:
+        | (({ loading }: { loading?: boolean }) => JSX.Element)
+        | FC<{ loading?: boolean }>;
+    Result?:
+        | (({ error }: { error?: Error }) => JSX.Element)
+        | FC<{ error?: Error }>;
 };
 
 export type Component<T> = FC<{ Response: T }>;
@@ -16,6 +21,7 @@ export default function Ajax<T>({
     Request,
     Component,
     Waiting,
+    Result,
 }: AjaxProp<T>): JSX.Element {
     const [[response, loading, error], Fetch, Catch] = useFetchJson<T>(Request);
     useEffect(() => {
@@ -24,7 +30,7 @@ export default function Ajax<T>({
     if (response) {
         return <Component Response={response} />;
     } else if (error) {
-        return <AjaxError error={error} />;
+        return Result ? <Result error={error} /> : <AjaxError error={error} />;
     } else {
         return Waiting ? <Waiting loading={loading} /> : <Skeleton />;
     }
