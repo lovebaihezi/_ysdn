148db69177f16750cebccb3a5a82d14d9b9b2b19
lovebaihezi <x18739168862@gmail.com>
2021-04-18 20:37:18 +0800

    add todo

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//build uml for application!
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(5050);
