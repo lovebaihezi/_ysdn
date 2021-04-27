3c8d2d345931759cb50fb4b042d776ea81a42027
lovebaihezi <x18739168862@gmail.com>
2021-04-27 19:27:21 +0800

    fix userlink bug

--- vite-ysdn/src/component/userLink/index.tsx
+++ vite-ysdn/src/component/userLink/index.tsx
@@ -5,8 +5,8 @@ const UserLink: FC<{
     user: { username: string; avatarUrl?: string; nickname: string };
 }> = ({ user }) => (
     <AvatarLink
-        src={user.avatarUrl}
-        name={user.nickname}
+        src={user?.avatarUrl}
+        name={user?.nickname}
     />
 );
 export default UserLink;
