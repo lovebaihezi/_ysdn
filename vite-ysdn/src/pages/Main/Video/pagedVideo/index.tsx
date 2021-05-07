6261f22f4752b9e34aa18c0adad45381dbf56ba8
lovebaihezi <x18739168862@gmail.com>
2021-05-07 09:46:13 +0800

    baseurl

--- vite-ysdn/src/pages/Main/Video/pagedVideo/index.tsx
+++ vite-ysdn/src/pages/Main/Video/pagedVideo/index.tsx
@@ -15,6 +15,7 @@ import {
 import { baseurl, useUserDetail } from '../../../../auth';
 import Tags from '../../../../component/ActionTags';
 import UserLink from '../../../../component/UserLink';
+import { Link } from 'react-router-dom';
 
 const PagedVideos: Component<AjaxJson.video[]> = ({ Response }) => {
     const [user] = useUserDetail();
@@ -31,57 +32,67 @@ const PagedVideos: Component<AjaxJson.video[]> = ({ Response }) => {
                 <Row>
                     {Response.map((video) => (
                         <Col span={8} style={{ padding: 36 }} key={video._id}>
-                            <Card
-                                actions={[
-                                    <LikeButton
-                                        type="video"
-                                        amount={
-                                            video.approval - video.disapproval
+                            <Link to={`/video/${video._id}`}>
+                                <Card
+                                    actions={[
+                                        <LikeButton
+                                            type="video"
+                                            amount={
+                                                video.approval -
+                                                video.disapproval
+                                            }
+                                            initial={
+                                                user
+                                                    ? user.like.videos.includes(
+                                                          video._id,
+                                                      )
+                                                    : false
+                                            }
+                                            id={video._id}
+                                        />,
+                                        <MarkButton
+                                            amount={video.markAmount}
+                                            type={'article'}
+                                            initial={
+                                                user
+                                                    ? user.marks.includes(
+                                                          video._id,
+                                                      )
+                                                    : false
+                                            }
+                                            id={video._id}
+                                        />,
+                                        <CommentButton
+                                            amount={video.commentsAmount}
+                                            link={`/video/${video._id}/#comment`}
+                                        />,
+                                        <ReadButton
+                                            amount={video.read}
+                                            link={`/video/${video._id}`}
+                                        />,
+                                    ]}
+                                    cover={<img src={video.coverImgUrl} />}
+                                    headStyle={{ border: 0 }}
+                                    bodyStyle={{ border: 0 }}
+                                    style={{
+                                        cursor: 'pointer',
+                                        marginBottom: 24,
+                                    }}
+                                >
+                                    <Card.Meta
+                                        title={video.title}
+                                        avatar={
+                                            <UserLink user={video.author} />
                                         }
-                                        initial={
-                                            user
-                                                ? user.like.videos.includes(
-                                                      video._id,
-                                                  )
-                                                : false
-                                        }
-                                        id={video._id}
-                                    />,
-                                    <MarkButton
-                                        amount={video.markAmount}
-                                        type={'article'}
-                                        initial={
-                                            user
-                                                ? user.marks.includes(video._id)
-                                                : false
-                                        }
-                                        id={video._id}
-                                    />,
-                                    <CommentButton
-                                        amount={video.commentsAmount}
-                                        link={`/video/${video._id}/#comment`}
-                                    />,
-                                    <ReadButton
-                                        amount={video.read}
-                                        link={`/video/${video._id}`}
-                                    />,
-                                ]}
-                                cover={<img src={video.coverImgUrl} />}
-                                headStyle={{ border: 0 }}
-                                bodyStyle={{ border: 0 }}
-                                style={{ cursor: 'pointer', marginBottom: 24 }}
-                            >
-                                <Card.Meta
-                                    title={video.title}
-                                    avatar={<UserLink user={video.author} />}
-                                    description={video.briefIntro}
-                                />
-                                <Row>
-                                    <Col span={24}>
-                                        <Tags tags={video.tags} />,
-                                    </Col>
-                                </Row>
-                            </Card>
+                                        description={video.briefIntro}
+                                    />
+                                    <Row>
+                                        <Col span={24}>
+                                            <Tags tags={video.tags} />,
+                                        </Col>
+                                    </Row>
+                                </Card>
+                            </Link>
                         </Col>
                     ))}
                 </Row>
